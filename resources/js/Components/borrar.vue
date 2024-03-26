83ab19fdaabe542dd13ba0f08858c9fb63bfa60b
Jorge Alvarez <jorgehaq@gmail.com>
2024-03-25 21:14:00 -0500

    first commit

---
+++ resources/js/Components/borrar.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <!-- TODO:borrar este archivo -->
+    
+</template>
+
+<template>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+
+    <head title="HOME"></head>
+
+    <div class="bg-green-500">HOME.VUE</div>
+    
+</template>
+
+<style>
+
+</style>
